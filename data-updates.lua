821053c3e0da8c648433e95009fd4e0cf96a4648
Bouncebag <lewis.jon.wright@gmail.com>
2023-01-08 19:49:59 +0000

    1.0.6
    If Artisanal Reskins: Bob's Mods found uses icons in their style

--- data-updates.lua
+++ data-updates.lua
@@ -26,23 +26,63 @@ if settings.startup["SteamBurnerInster-burnerstackupdate"].value == true then
 	end
 end
 
-if data.raw.item["steam-stack-inserter"]  then 
-	data.raw.item["steam-stack-inserter"].icon = "__SteamBurnerInster__/graphics/icons/white-stack-inserter.png"
-    data.raw.item["steam-stack-inserter"].icon_size = 32
+-- Artisanal Reskins found, use icons in their style
+if mods["reskins-bobs"] then
+	if data.raw.item["steam-stack-inserter"]  then 
+		data.raw.item["steam-stack-inserter"].icon = "__SteamBurnerInster__/graphics/icons/reskins/white-stack-inserter.png"
+		data.raw.item["steam-stack-inserter"].icon_size = 64
+		data.raw.item["steam-stack-inserter"].icon_mipmaps = 4
+	end
+	if data.raw.item["steam-stack-filter-inserter"]  then 
+		data.raw.item["steam-stack-filter-inserter"].icon = "__SteamBurnerInster__/graphics/icons/reskins/white-stack-filter-inserter.png"
+		data.raw.item["steam-stack-filter-inserter"].icon_size = 64
+		data.raw.item["steam-stack-filter-inserter"].icon_mipmaps = 4
+	end
+	if data.raw.item["steam-filter-inserter"]  then 
+		data.raw.item["steam-filter-inserter"].icon = "__SteamBurnerInster__/graphics/icons/reskins/white-filter-inserter.png"
+		data.raw.item["steam-filter-inserter"].icon_size = 64
+		data.raw.item["steam-filter-inserter"].icon_mipmaps = 4
+	end
+		if data.raw.item["steam-inserter"]  then 
+		data.raw.item["steam-inserter"].icon = "__SteamBurnerInster__/graphics/icons/reskins/white-inserter.png"
+		data.raw.item["steam-inserter"].icon_size = 64
+		data.raw.item["steam-inserter"].icon_mipmaps = 4
+	end
+	if data.raw.item["burner-stack-inserter"]  then 
+		data.raw.item["burner-stack-inserter"].icon = "__SteamBurnerInster__/graphics/icons/reskins/burner-stack-inserter.png"
+		data.raw.item["burner-stack-inserter"].icon_size = 64
+		data.raw.item["burner-stack-inserter"].icon_mipmaps = 4
+	end
+	if data.raw.item["burner-stack-filter-inserter"]  then 
+		data.raw.item["burner-stack-filter-inserter"].icon = "__SteamBurnerInster__/graphics/icons/reskins/burner-stack-filter-inserter.png"
+		data.raw.item["burner-stack-filter-inserter"].icon_size = 64
+		data.raw.item["burner-stack-filter-inserter"].icon_mipmaps = 4
+	end
+	if data.raw.item["burner-filter-inserter"]  then 
+		data.raw.item["burner-filter-inserter"].icon = "__SteamBurnerInster__/graphics/icons/reskins/burner-filter-inserter.png"
+		data.raw.item["burner-filter-inserter"].icon_size = 64
+		data.raw.item["burner-filter-inserter"].icon_mipmaps = 4
+	end
+else --Use standard bobs style icons
+	if data.raw.item["steam-stack-inserter"]  then 
+		data.raw.item["steam-stack-inserter"].icon = "__SteamBurnerInster__/graphics/icons/white-stack-inserter.png"
+		data.raw.item["steam-stack-inserter"].icon_size = 32
+	end
+	if data.raw.item["steam-stack-filter-inserter"]  then 
+		data.raw.item["steam-stack-filter-inserter"].icon = "__SteamBurnerInster__/graphics/icons/white-stack-filter-inserter.png"
+		data.raw.item["steam-stack-filter-inserter"].icon_size = 32
+	end
+	if data.raw.item["steam-filter-inserter"]  then 
+		data.raw.item["steam-filter-inserter"].icon = "__SteamBurnerInster__/graphics/icons/white-filter-inserter.png"
+		data.raw.item["steam-filter-inserter"].icon_size = 32
+	end
+	if data.raw.item["burner-stack-inserter"]  then 
+		data.raw.item["burner-stack-inserter"].icon = "__SteamBurnerInster__/graphics/icons/burner-stack-inserter.png"
+		data.raw.item["burner-stack-inserter"].icon_size = 32
+	end
+	if data.raw.item["burner-stack-filter-inserter"]  then 
+		data.raw.item["burner-stack-filter-inserter"].icon = "__SteamBurnerInster__/graphics/icons/burner-stack-filter-inserter.png"
+		data.raw.item["burner-stack-filter-inserter"].icon_size = 32
+	end
 end
-if data.raw.item["steam-stack-filter-inserter"]  then 
-	data.raw.item["steam-stack-filter-inserter"].icon = "__SteamBurnerInster__/graphics/icons/white-stack-filter-inserter.png"
-    data.raw.item["steam-stack-filter-inserter"].icon_size = 32
-end
-if data.raw.item["steam-filter-inserter"]  then 
-	data.raw.item["steam-filter-inserter"].icon = "__SteamBurnerInster__/graphics/icons/white-filter-inserter.png"
-    data.raw.item["steam-filter-inserter"].icon_size = 32
-end
-if data.raw.item["burner-stack-inserter"]  then 
-	data.raw.item["burner-stack-inserter"].icon = "__SteamBurnerInster__/graphics/icons/burner-stack-inserter.png"
-    data.raw.item["burner-stack-inserter"].icon_size = 32
-end
-if data.raw.item["burner-stack-filter-inserter"]  then 
-	data.raw.item["burner-stack-filter-inserter"].icon = "__SteamBurnerInster__/graphics/icons/burner-stack-filter-inserter.png"
-    data.raw.item["burner-stack-filter-inserter"].icon_size = 32
-end
+
